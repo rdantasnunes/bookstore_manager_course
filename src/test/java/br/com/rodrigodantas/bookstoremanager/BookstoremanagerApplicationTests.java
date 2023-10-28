752d72a657230b71f0f31b3c252e5ec4cec46a41
rdantasnunes <rdantasnunes@gmail.com>
2023-10-28 12:57:48 -0300

    primeiro commit

---
+++ src/test/java/br/com/rodrigodantas/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigodantas.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
